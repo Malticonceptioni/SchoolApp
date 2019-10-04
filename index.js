c4364507988b4eb2bfe930f32d6cd5a56165138e
Mona <mona.conceptioni@gmail.com>
2019-10-04 17:24:31 +0530

    Second Commit

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+//solution for three finger screenshot
 console.disableYellowBox = true;
 AppRegistry.registerComponent(appName, () => App);
